321260fd8f9a347c6b69cc6cdacd7ab931b157e4
candre1 <candre1@umbc.edu>
2017-11-28 15:21:47 -0500

    Addition to packet_parser
    Added Guide string and Local string.  Added some commented-out functionality that we should maybe focus on. (?)

--- packet_parser.py
+++ packet_parser.py
@@ -18,49 +18,86 @@
 #               data based upon the decided string
 #               format.
 #######################################################
-def string_parser(s):
+def string_parser(guideString, localString):
     
-    # Separate Location Data
-    AvePtpFlag = s[0]
-    LeaderFollowerFlag = s[1]
-    PositionInConvoy = s[2:81]
-    EmergencyStop = s[82]
-    Location = s[83:112]
-    LineOfBearing = s[113:128]
-    VelocityX = s[129:160]
-    VelocityY = s[161:192]
-    VelocityZ = s[193:224]
-    AccelerationX = s[225:256]
-    AccelerationY = s[257:288]
-    AccelerationZ = s[289:320]
-    extraData = s[321:]
+    # Separate Guide Data
+    GuideAvePtpFlag = guideString[0]
+    GuideLeaderFollowerFlag = guideString[1]
+    GuidePositionInConvoy = guideString[2:81]
+    GuideEmergencyStop = guideString[82]
+    GuideLocation = guideString[83:112]
+    GuideLineOfBearing = guideString[113:128]
+    GuideVelocityX = guideString[129:160]
+    GuideVelocityY = guideString[161:192]
+    GuideVelocityZ = guideString[193:224]
+    GuideAccelerationX = guideString[225:256]
+    GuideAccelerationY = guideString[257:288]
+    GuideAccelerationZ = guideString[289:320]
+    GuideExtraData = guideString[321:]
+
+    # Separate Local Data
+    LocalAvePtpFlag = localString[0]
+    LocalLeaderFollowerFlag = localString[1]
+    LocalPositionInConvoy = localString[2:81]
+    LocalEmergencyStop = localString[82]
+    LocalLocation = localString[83:112]
+    LocalLineOfBearing = localString[113:128]
+    LocalVelocityX = localString[129:160]
+    LocalVelocityY = localString[161:192]
+    LocalVelocityZ = localString[193:224]
+    LocalAccelerationX = localString[225:256]
+    LocalAccelerationY = localString[257:288]
+    LocalAccelerationZ = localString[289:320]
+    LocalExtraData = localString[321:]
 
     # Print Data
-    print(AvePtpFlag)
-    print(LeaderFollowerFlag)
-    print(PositionInConvoy)
-    print(EmergencyStop)
-    print(Location)
-    print(LineOfBearing)
-    print(VelocityX)
-    print(VelocityY)
-    print(VelocityZ)
-    print(AccelerationX)
-    print(AccelerationY)
-    print(AccelerationZ)
-    print(extraData)
+    print("GuideAvePtpFlag: " + GuideAvePtpFlag)
+    print("LocalAvePtpFlag: " + LocalAvePtpFlag)
+    print("GuideLeaderFollowerFlag: " + GuideLeaderFollowerFlag)
+    print("LocalLeaderFollowerFlag: " + LocalLeaderFollowerFlag)
+    print("GuidePositionInConvoy: " + GuidePositionInConvoy)
+    print("LocalPositionInConvoy: " + LocalPositionInConvoy)
+    print("GuideEmergencyStop: " + GuideEmergencyStop)
+    print("LocalEmergencyStop: " + LocalEmergencyStop)
+    print("GuideLocation: " + GuideLocation)
+    print("LocalLocation: " + LocalLocation)
+    print("GuideLineOfBearing: " + GuideLineOfBearing)
+    print("LocalLineOfBearing: " + LocalLineOfBearing)
+    print("GuideVelocityX: " + GuideVelocityX)
+    print("LocalVelocityX: " + LocalVelocityX)
+    print("GuideVelocityY: " + GuideVelocityY)
+    print("LocalVelocityY: " + LocalVelocityY)
+    print("GuideVelocityZ: " + GuideVelocityZ)
+    print("LocalVelocityZ: " + LocalVelocityZ)
+    print("GuideAccelerationX: " + GuideAccelerationX)
+    print("LocalAccelerationX: " + LocalAccelerationX)
+    print("GuideAccelerationY: " + GuideAccelerationY)
+    print("LocalAccelerationY: " + LocalAccelerationY)
+    print("GuideAccelerationZ: " + GuideAccelerationZ)
+    print("LocalAccelerationZ: " + LocalAccelerationZ)
+    print("GuideExtraData: " + GuideExtraData)
+    print("LocalExtraData: " + LocalExtraData)
+
+    # Calculate Controls
+    #get_motor_commands(someInputs)
 
 
 ## main ###############################################
 # Inputs:       N/A
 # Outputs:      N/A
-# Description:  Simply takes "inString" and passes it
-#               int0 the function "string_parser".
-#               (to be updated...)
+# Description:  Simply takes "inString" and "outString"
+#               and passes them into the function
+#               "string_parser". (to be updated...)
 #######################################################
+
 def main():
-    inString = "1010101010101010101010101010101010101010101010101010101010101011010101010101010101010101010101010101010101010101010101010110101101010101010101010101010101010101010101010101101011010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101"
-    string_parser(inString)
+    #while (1):
+    #outString = get_local_data(localPTPaddress)
+    outString = "0010101010101010101010101010101010101010101010101010101010101011010101010101010101010101010101010101010101010101010101010110101101010101010101010101010101010101010101010101101011010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101"
+    #send_local_data(outString)
+    #inString = get_guide_data(WIFIaddress)
+    inString = "0110101010101010101010101010101010101010101010101010101010101011010101010101010101010101010101010101010101010101010101010110101101010101010101010101010101010101010101010101101011010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101"
+    string_parser(inString, outString)
 
 main()
     
